b18e29dbb9b7c4d1be6e7414f1f936a193742b8b
Samuel Lassoie <samuel.lassoie@manganelli.com>
2019-01-10 18:17:17 +0100

    wip

--- project/apiEcole/index.js
+++ project/apiEcole/index.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+var cors = require('cors');
 
 const app = express();
+
+// partage des ressources entre origines multiples
+//https://developer.mozilla.org/fr/docs/Web/HTTP/CORS
+app.use(cors());
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+//le tableau des utilisateurs
 const users = [
     {id: 1, firstname: 'Roger', lastname: 'Véro', weight: 70.5},
     {id: 2, firstname: 'David', lastname: 'andrew', weight: 70.5},
@@ -18,9 +31,90 @@ app.get('/', function (req, res) {
     res.send('Hello World!')
 });
 
+/**
+ * Route retournant la liste des utilisateurs
+ */
 app.get('/users', function (req, res) {
     res.status(200).json(users);
 });
+
+/**
+ * Route permettant de récuperer les informations d'un utilisateur
+ */
+app.get('/users/:id', function (req, res) {
+    //on récupère le paramètre :id
+    const id = req.params.id;
+
+
+    //On recherche un utilisateur dans le tableau avec son id
+    const user = users.find(function (u) {
+        return u.id == id;
+    });
+    //const user = users.find(u => u.id == id); => VERSION AVEC UNE FONCTION FLECHEE
+
+
+    //Si aucun résultat on renvoie un JSON avec un code d'erreur 404: utilisateur non trouvé
+    if(!user) {
+        return res.status(404).json({error: "user not found."});
+    }
+    //Si pas d'erreur on renvoie le resultat
+    res.status(200).json(user);
+});
+
+
+/**
+ * Route permettant de supprimer un utilisateur
+ */
+app.delete('/users/:id', function (req, res) {
+    //on récupère le paramètre :id
+    const id = req.params.id;
+
+    //Le findIndex va nous permettre de recherche la position de l'utilisateur dans le tableau
+    const index = users.findIndex(function (u) {
+        return u.id == id;
+    });
+    //const index = users.findIndex(u => u.id == id); => VERSION AVEC UNE FONCTION FLECHEE
+
+    //Si aucun résultat on renvoie un JSON avec un code d'erreur 404: utilisateur non trouvé
+    if(-1 === index) {
+        return res.status(404).json({error: "user not found."});
+    }
+
+    //On supprime l'utilisateur du tableau
+    users.splice(index, 1);
+
+    //on renvoie un code http 204 pour indiquer que tout s'est bien passé mais qu'il n'y a aucun resultat
+    res.status(204).json();
+});
+
+
+/**
+ * Route permettant de mettre à jour les informations d'un utilisateur
+ */
+app.post('/users/:id', function (req, res) {
+    //on récupère le paramètre :id
+    const id = req.params.id;
+
+    //On recherche un utilisateur dans le tableau avec son id
+    const user = users.find(function (u) {
+        return u.id == id;
+    });
+    //  const user = users.find(u => u.id == id); => VERSION AVEC UNE FONCTION FLECHEE
+
+    //Si aucun résultat on renvoie un JSON avec un code d'erreur 404: utilisateur non trouvé
+    if(!user) {
+        return res.status(404).json({error: "user not found."});
+    }
+
+    //on met à jour les informations de l'utilisateur
+    user.firstname = req.body.firstname || user.firstname;
+    user.lastname = req.body.lastname || user.lastname;
+    user.weight = parseFloat(req.body.weight || user.weight);
+
+    //on renvoie un code http 200 pour dire que tout s'est bien passé + l'objet utilisateur mis à jour
+    res.status(200).json(user);
+});
+
 
 app.get('/test', function (req, res) {
     const voiture = req.query.voiture || 'N/A';
