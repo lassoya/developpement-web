c72d5e2519c20916948e975aca5235a634ac56ff
Samuel Lassoie <samuel.lassoie@manganelli.com>
2019-01-31 19:25:34 +0100

    wip

--- inscription-ecole/index.js
+++ inscription-ecole/index.js
@@ -21,22 +21,36 @@ app.get('/formations', async function (req, res) {
 */
 
 
-app.get('/formations', function (req, res) {
-  db.formation.findAll().then(function(data){
-     res.status(200).json(data);
-  });
+app.get('/api/formations', function (req, res) {
+    db.formation.findAll().then(function (data) {
+        res.status(200).json(data);
+    });
 });
 
-app.get('/formations/:id', function(req, res){
+app.get('/api/formations/:id', function (req, res) {
     const id = req.params.id;
-    db.formation.findById(id).then(function(formation){
-        if(!formation){
+    db.formation.findById(id).then(function (formation) {
+        if (!formation) {
             return res.status(404).json({error: 'formation not found.'});
         }
         res.status(200).json(formation);
     });
 });
 
+app.post('/api/formations', function (req, res) {
+    const data = {
+        label: req.body.label || null,
+        description: req.body.description || null,
+        start_date: req.body.start_date || null,
+        end_date: req.body.end_date || null,
+        cost: req.body.cost || null
+    };
+
+    const formation = db.formation.build(data);
+    formation.save().then(function (data) {
+        res.status(200).json(data);
+    });
+});
 
 
 app.listen(4501, function () {
