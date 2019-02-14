5face6315e32b760d3165901ff02378537a4fc97
Samuel Lassoie <samuel.lassoie@manganelli.com>
2019-02-14 18:33:47 +0100

    init formation

--- inscription-ecole/frontend/assets/js/app.js
+++ inscription-ecole/frontend/assets/js/app.js
@@ -5,14 +5,24 @@ $(document).ready(function () {
         $(this).addClass('active');
     });
 
+    function initPage(hash) {
+        if (hash === 'formation') {
+            initFormation();
+        }
+    }
 
-    
+    function initFormation() {
+
+
+
+    }
+
 
     $(window).bind('hashchange', function () { //detect hash change
         var hash = window.location.hash.slice(1); //hash to string (= "myanchor")
         $('.detail-page').hide();
         if (hash) {
-            $('#' + hash).show();
+            initPage(hash);
         }
     });
 
