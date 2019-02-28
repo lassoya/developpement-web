16168599ebfa44d48b020919f4ee7402bd7b98f3
Samuel Lassoie <samuel.lassoie@manganelli.com>
2019-02-28 19:30:07 +0100

    wip

--- inscription-ecole/frontend/assets/js/formation.js
+++ inscription-ecole/frontend/assets/js/formation.js
@@ -1,25 +1,38 @@
 const formation = {};
 
-formation.supprimer = function (id) {
-    console.log('yooo', id);
+formation.supprimer = function (id, index) {
+    $.ajax({
+        url: '/api/formations/' + id,
+        method: 'DELETE'
+    }).done(function (formations) {
+        formation.data.splice(index, 1);
+        formation.refreshTable();
+    });
 };
+
+formation.data = [];
 
 formation.init = function () {
     $.ajax({
         url: '/api/formations',
         type: 'GET'
     }).done(function (formations) {
+        formation.data = formations;
         $('#formation').show();
-        $('#formation table tbody').empty();
-        formations.forEach(function (formation) {
-            $('#formation table tbody').append(
-                '<tr><td>' + formation.label + '</td>' +
-                '<td>' + formation.description + '</td>' +
-                '<td>' + formation.start_date + '</td>' +
-                '<td>' + formation.end_date + '</td>' +
-                '<td>' + formation.cost + '</td>' +
-                '<td><button onclick="formation.supprimer(' + formation.id + ')" class="btn btn-danger">Supprimer</button></td>' +
-                '</tr>');
-        });
+        formation.refreshTable();
     });
 };
+
+formation.refreshTable = function () {
+    $('#formation table tbody').empty();
+    formation.data.forEach(function (formation, index) {
+        $('#formation table tbody').append(
+            '<tr><td>' + formation.label + '</td>' +
+            '<td>' + formation.description + '</td>' +
+            '<td>' + formation.start_date + '</td>' +
+            '<td>' + formation.end_date + '</td>' +
+            '<td>' + formation.cost + '</td>' +
+            '<td><button onclick="formation.supprimer(' + formation.id + ', ' + index + ')" class="btn btn-danger">Supprimer</button></td>' +
+            '</tr>');
+    });
+}
